f648d339d69df406e925b688d5e24b484ea96323
Halyna Moldovianu <Beorgeos@gmail.com>
2025-06-09 14:16:17 +0200

    env added,mongo connection function added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { pinoHttp } from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
 
 export const setupServer = () => {
   const app = express();
