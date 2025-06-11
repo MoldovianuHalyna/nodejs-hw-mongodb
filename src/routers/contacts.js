a6afe5dc47c9c087adbe83102c44785aa7052f0b
Halyna Moldovianu <Beorgeos@gmail.com>
2025-06-11 11:40:57 +0200

    controllers added

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,34 +1,13 @@
-import { getContactById, getContacts } from '../services/contacts.js';
 import { Router } from 'express';
+import {
+  getContactByIdController,
+  getContactController,
+} from '../controllers/contactsControllers.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', async (req, res) => {
-  const contactsData = await getContacts();
+contactsRouter.get('/', getContactController);
 
-  res.json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    contactsData,
-  });
-});
-
-contactsRouter.get('/:contactId', async (req, res) => {
-  const { contactId } = req.params;
-
-  const contactData = await getContactById(contactId);
-
-  if (!contactData) {
-    return res.status(404).json({
-      message: 'Contact not found',
-    });
-  }
-
-  res.json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    contactData,
-  });
-});
+contactsRouter.get('/:contactId', getContactByIdController);
 
 export default contactsRouter;
