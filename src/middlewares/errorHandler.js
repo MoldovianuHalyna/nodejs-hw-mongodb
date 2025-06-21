d1d494b21e8dc270d92a92d7cdcfc09d6e03d3e5
Halyna Moldovianu <Beorgeos@gmail.com>
2025-06-21 17:33:18 +0200

    validation finished

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,8 +4,7 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err,
+      message: err.message,
     });
     return;
   }
